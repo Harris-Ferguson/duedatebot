4c7b297125be383faf00dfed6b9552045be2889b
Harris <haf094@usask.ca>
2020-10-09 20:42:44 -0600

    added admin restriction checking event

--- bot.py
+++ bot.py
@@ -15,17 +15,19 @@ cluster = MongoClient("mongodb+srv://duckypotato:" + DBPASS + "@cluster0.bore2.m
 db = cluster["duedates"]
 collection = db["duedates"]
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='@')
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-@bot.command(name="duedates")
-async def duedate(ctx):
-    await ctx.send("hello!")
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send("```Someone is trying something illegal! (you need admin for this command)```")
 
 @bot.command(name="add-due-date", help="Adds a due date to the list of due dates.\n arg1: name arg2: class arg3: date due format: MON D YYYY EXAMPLE: Jun 1 2020")
+@commands.has_role("admin")
 async def duedate(ctx, arg1, arg2, arg3):
     duedatetime = datetime.strptime(arg3, '%b%d%Y')
     post_data = {
@@ -35,22 +37,32 @@ async def duedate(ctx, arg1, arg2, arg3):
     }
     result = collection.insert_one(post_data)
     print('One post:{0}'.format(result.inserted_id))
-    await ctx.send("Added Due Date for: " + arg1 + "\nClass:  " + arg2 + "\nDue on: " + duedatetime.strftime('%b %d %Y'))
+    await ctx.send("```Added Due Date for: " + arg1 + "\nClass:  " + arg2 + "\nDue on: " + duedatetime.strftime('%b %d %Y') + "```")
 
-@bot.command(name="list-due-dates", help="Lists all due dates")
+@bot.command(name="dates", help="Lists all due dates")
 async def listdue(ctx):
     dates = []
     for post in collection.find():
-        dates.append(post["class"] + " " + post["name"] + " Due On: " + post["duedate"].strftime('%b %d %Y'))
+        dates.append("```" + post["class"] + " " + post["name"] + " Due On: " + post["duedate"].strftime('%b %d %Y') + "```")
 
     for date in dates:
         await ctx.send(date)
 
-@bot.command(name="datesforclass", help="Lists all the due dates for a specified course")
+@bot.command(name="datesfor", help="Lists all the due dates for a specified course")
 async def listdue(ctx, arg1):
     dates = []
     for post in collection.find({"class":arg1}):
-        dates.append(post["class"] + " " + post["name"] + " Due On: " + post["duedate"].strftime('%b %d %Y'))
+        dates.append("```" + post["class"] + " " + post["name"] + " Due On: " + post["duedate"].strftime('%b %d %Y') + "```")
+
+    for date in dates:
+        await ctx.send(date)
+
+@bot.command(name="duetoday", help="Lists everything due today")
+async def todaydue(ctx):
+    dates = []
+    for post in collection.find():
+        if post["duedate"].date() == datetime.today().date():
+            dates.append("```" + post["class"] + " " + post["name"] + " Due On: " + post["duedate"].strftime('%b %d %Y') + "```")
 
     for date in dates:
         await ctx.send(date)
