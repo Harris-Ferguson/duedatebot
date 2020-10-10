81556944b511b8fb849fc9723b528fab89a98793
Harris <haf094@usask.ca>
2020-10-09 22:28:52 -0600

    database tables sort by guild:

--- bot.py
+++ bot.py
@@ -20,17 +20,22 @@ bot = commands.Bot(command_prefix='@')
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
+    for guild in bot.guilds:
+        print(guild.id)
 
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send("```Someone is trying something illegal! (you need admin for this command)```")
 
-@bot.command(name="add-due-date", help="Adds a due date to the list of due dates.\n arg1: name arg2: class arg3: date due format: MON D YYYY EXAMPLE: Jun 1 2020")
+@bot.command(name="adddate", help="Adds a due date to the list of due dates.\n arg1: name arg2: class arg3: date due format: MON D YYYY EXAMPLE: Jun 1 2020")
 @commands.has_role("admin")
 async def duedate(ctx, arg1, arg2, arg3):
     duedatetime = datetime.strptime(arg3, '%b%d%Y')
+    guild = ctx.guild.id
+    print(guild)
     post_data = {
+        'guild': guild,
         'name': arg1,
         'class': arg2,
         'duedate': duedatetime
@@ -42,7 +47,8 @@ async def duedate(ctx, arg1, arg2, arg3):
 @bot.command(name="dates", help="Lists all due dates")
 async def listdue(ctx):
     dates = []
-    for post in collection.find():
+    guild = ctx.guild.id
+    for post in collection.find({"guild": guild}):
         dates.append("```" + post["class"] + " " + post["name"] + " Due On: " + post["duedate"].strftime('%b %d %Y') + "```")
 
     for date in dates:
@@ -51,7 +57,8 @@ async def listdue(ctx):
 @bot.command(name="datesfor", help="Lists all the due dates for a specified course")
 async def listdue(ctx, arg1):
     dates = []
-    for post in collection.find({"class":arg1}):
+    guild = ctx.guild.id
+    for post in collection.find({"guild": guild ,"class":arg1}):
         dates.append("```" + post["class"] + " " + post["name"] + " Due On: " + post["duedate"].strftime('%b %d %Y') + "```")
 
     for date in dates:
@@ -60,7 +67,8 @@ async def listdue(ctx, arg1):
 @bot.command(name="duetoday", help="Lists everything due today")
 async def todaydue(ctx):
     dates = []
-    for post in collection.find():
+    guild = ctx.guild.id
+    for post in collection.find({"guild": guild}):
         if post["duedate"].date() == datetime.today().date():
             dates.append("```" + post["class"] + " " + post["name"] + " Due On: " + post["duedate"].strftime('%b %d %Y') + "```")
 
