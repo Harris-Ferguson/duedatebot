830824a40016bd154881e7173d4655a717ceaaeb
Harris <haf094@usask.ca>
2021-01-10 16:59:59 -0600

    added command to create a new google calendar with all upcoming events

--- app/calandar.py
+++ app/calandar.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-import datetime
+from datetime import datetime, timedelta
 import pickle
 import os.path
 from googleapiclient.discovery import build
@@ -13,12 +13,13 @@ import helpers
 from storage import Storage
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 class Calandar(commands.Cog):
         def __init__(self, bot):
             self.bot = bot
             self.storage = Storage(bot)
+            self.creds = self.get_creds()
 
         def get_creds(self):
             creds = None
@@ -37,20 +38,44 @@ class Calandar(commands.Cog):
                     pickle.dump(creds, token)
             return creds
 
-        @commands.command(name="createcalendar")
-        async def create_cal(self, ctx):
-            creds = self.get_creds()
-            service = build('calendar', 'v3', credentials=creds)
-            now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-            events_result = service.events().list(calendarId='primary', timeMin=now,
-                                                maxResults=10, singleEvents=True,
+        @commands.command(name="createcalendar", help="arg1: name")
+        async def create_cal(self, ctx, name):
+            # call the google calendar API
+            service = build('calendar', 'v3', credentials=self.creds)
+            # get the list of reminders
+            posts = await self.storage.get_posts(ctx.guild.id)
+
+            # create the calendar first
+            calendar = {
+            "summary": name,
+            "timeZone": "Canada/Saskatchewan" #TO-DO change this to read from the ctx
+            }
+            created_calender = service.calendars().insert(body=calendar).execute()
+
+            for post in posts:
+                # write all the posts as events in the calendar
+                start_date = post["duedate"].strftime('%Y-%m-%d')
+                end_date = (post["duedate"] + timedelta(days=1)).strftime('%Y-%m-%d')
+                print(start_date, end_date)
+                event = {
+                    'summary': post["class"] + " " + post["name"],
+                    'start': {
+                        'date': start_date,
+                        'timeZone': "Canada/Saskatchewan"
+                    },
+                    'end': {
+                        'date': end_date,
+                        'timeZone': "Canada/Saskatchewan"
+                    }
+                }
+                event = service.events().insert(calendarId=created_calender['id'], body=event).execute()
+            # get the first event's url as a link to send to the context
+            now = datetime.utcnow().isoformat() + 'Z'
+            events_result = service.events().list(calendarId=created_calender['id'], timeMin=now,
+                                                maxResults=1, singleEvents=True,
                                                 orderBy='startTime').execute()
             events = events_result.get('items', [])
-
-            if not events:
-                await ctx.send("No events found!")
-            for event in events:
-                await ctx.send(str(event["summary"]))
+            await ctx.send("Created a new calendar! \n {0}".format(events[0].get('htmlLink')))
 
 def setup(bot):
     b = Calandar(bot)
